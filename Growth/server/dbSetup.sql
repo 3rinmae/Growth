ec27870bd8290ca16a73dcb95d2d94cf3715be24
Erin Shiffler <erinmaeshiffler@gmail.com>
2024-02-01 17:51:57 -0700

    project table working

--- Growth/server/dbSetup.sql
+++ Growth/server/dbSetup.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key', createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created', updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update', name varchar(255) COMMENT 'User Name', email varchar(255) COMMENT 'User Email', picture varchar(255) COMMENT 'User Picture'
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS projects (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, name VARCHAR(255) NOT NULL, description VARCHAR(1500) NOT NULL, creatorId VARCHAR(255) NOT NULL, Foreign Key (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
 ) default charset utf8 COMMENT '';
